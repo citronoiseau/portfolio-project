e16ccec4bc5b3ddbd5f567809ddda07dad31e5fb
citronoiseau <avroraka2004@gmail.com>
2023-11-01 23:37:14 +0200

    integrated smtp.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,28 @@
 const copyBtn = document.querySelector(`#copyToClipBoard`);
 
 function copyFunction() {
-  alert("Hi!");
+  navigator.clipboard.writeText(`anastasiiatrubachova1@gmail.com`);
+  alert("Email copied!");
 }
 copyBtn.addEventListener(`click`, copyFunction);
+
+const sendBtn = document.querySelector(`#btn-send`);
+
+function sendFunction(event) {
+  event.preventDefault();
+  const name = document.querySelector(`#name`).value;
+  const email = document.querySelector(`#email`).value;
+  const subject = document.querySelector(`#subject`).value;
+  const message = document.querySelector(`#message`).value;
+  const body =
+    `name: ` + name + `<br/> email:` + email + `<br/> message: ` + message;
+  Email.send({
+    SecureToken: "2c9b081e-8e5b-4029-b2cb-c006776d7a4a",
+    To: "anastasiiatrubachova1@gmail.com",
+    From: "anastasiiatrubachova1@gmail.com",
+    Subject: subject,
+    Body: body,
+  }).then((message) => alert("Message sent succesfully!"));
+}
+
+sendBtn.addEventListener(`click`, sendFunction);
